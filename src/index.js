216f4d361475c0b9955e30c3f506ca3ad61d1c6a
Paul Fadayo <paulfadayo@yahoo.com>
2023-07-03 14:00:00 +0100

    setup

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>Hello React!</h1>;
+}
+
+// React v18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+//  React Before 18
+//  React.render(<Ap />, document.getElementById("root"));
